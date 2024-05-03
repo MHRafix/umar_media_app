5e49a610ea57d8cd67af22583d0ca31b55745f7a
MHRafix <fmhrafiz27@gmail.com>
2024-05-03 12:39:07 +0600

    home banner set done

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:ttp_app/screens/home/utils/dataModel.dart';
-import 'package:ttp_app/widgets/common-widgets/shimmer-widgets.dart';
+
+class Slide {
+  Slide({
+    required this.title,
+    required this.height,
+    required this.color,
+  });
+
+  final Color color;
+  final double height;
+  final String title;
+}
+
+var slides = List.generate(
+  6,
+  (index) => Slide(
+    title: 'Slide ${index + 1}',
+    height: 230,
+    color: Colors.primaries[index % Colors.primaries.length],
+  ),
+);
+
+final List<Widget> sliders = slides
+    .map(
+      (item) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/images/placeholder_image.png',
+          image:
+              "https://res.cloudinary.com/coderxone/image/upload/v1710490450/travel_package_thumbnails/altwb94zewlaoxaqfbzz.jpg",
+          // width: 250,
+          // height: 100,
+          fit: BoxFit.cover,
+          placeholderFit: BoxFit.cover,
+          fadeInCurve: Easing.legacy,
+        ),
+      ),
+    )
+    .toList();
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
     return Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: PreferredSize(
@@ -48,7 +89,20 @@ class HomeScreen extends StatelessWidget {
           padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              const ShimmerWidget.rectangular(height: 200),
+              // const ShimmerWidget.rectangular(height: 200),
+              FlutterCarousel(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  disableCenter: true,
+                  viewportFraction: deviceSize.width > 800.0 ? 0.8 : 1.0,
+                  height: 230,
+                  indicatorMargin: 12.0,
+                  enableInfiniteScroll: true,
+                  slideIndicator: const CircularSlideIndicator(),
+                ),
+                items: sliders,
+              ),
               const SizedBox(
                 height: 20,
               ),
@@ -62,7 +116,7 @@ class HomeScreen extends StatelessWidget {
                     ),
                     itemCount: homeMenu.length,
                     itemBuilder: (context, i) {
-                      return _moduleButton(context, homeMenu[i]);
+                      return _gridModuleBox(context, homeMenu[i]);
                     }),
               ),
             ],
@@ -71,7 +125,7 @@ class HomeScreen extends StatelessWidget {
   }
 
   // _moduleButton
-  Container _moduleButton(BuildContext context, dynamic menu) {
+  Container _gridModuleBox(BuildContext context, dynamic menu) {
     return Container(
         // color: Colors.grey[200],
         decoration: BoxDecoration(
